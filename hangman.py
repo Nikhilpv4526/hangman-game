22f5266b473d587e2dcd9f867ed9c36ea41e419c
Nikhilpv4526 <pvnikhil618@gmail.com>
2025-07-23 15:18:32 +0530

    updated working game

--- hangman.py
+++ hangman.py
@@ -1,19 +1,34 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
+
 chosen_word = random.choice(word_list)
 print(chosen_word)
+
 placeholder = ""
-word_len= len(chosen_word)
-for position in range(word_len):
+word_length = len(chosen_word)
+for position in range(word_length):
     placeholder += "_"
 print(placeholder)
-guess = input("guess a letter?")
-guess.lower()
-display = ""
-for letter in chosen_word:
-    if letter == guess:
-        display += letter
-    else:
-        display += "_"
 
-print(display)     
+game_over = False
+correct_letters = []
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+
+    display = ""
+
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(letter)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print(display)
+
+    if "_" not in display:
+        game_over= True
+        print("you win!")
