23e0d5fb1f7180fc6223778bf09ca14944d5b68e
Nikhilpv4526 <pvnikhil618@gmail.com>
2025-07-23 14:58:48 +0530

    update placeholder code

--- hangman.py
+++ hangman.py
@@ -2,6 +2,11 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
+placeholder = ""
+word_len= len(chosen_word)
+for position in range(word_len):
+    placeholder += "_"
+print(placeholder)
 guess = input("guess a letter?")
 guess.lower()
 for letter in chosen_word:
