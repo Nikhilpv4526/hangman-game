e1096120cd8fe48b522bab95087df78604d5aa37
Nikhilpv4526 <pvnikhil618@gmail.com>
2025-07-23 14:47:08 +0530

    Initial commit of hangman game

---
+++ hangman.py
@@ -0,0 +1,11 @@
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+print(chosen_word)
+guess = input("guess a letter?")
+guess.lower()
+for letter in chosen_word:
+    if letter == guess:
+        print("right")
+    else:
+        print("wrong")                
